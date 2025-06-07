0060b6d79edddc075a105e4ec5bf9cd85ad54598
Mael-RABOT <mael.rabot@epitech.eu>
2025-06-07 15:58:34 +0200

    evol(i18n): change local path
    Signed-off-by: Mael-RABOT <mael.rabot@epitech.eu>

--- src/i18n/i18n.ts
+++ src/i18n/i18n.ts
@@ -3,20 +3,20 @@ import { initReactI18next } from 'react-i18next';
 import LanguageDetector from 'i18next-browser-languagedetector';
 
 // Import all translation files
-import enTranslations from '../../public/locales/en/en.json';
-import frTranslations from '../../public/locales/fr/fr.json';
-import enHome from '../../public/locales/en/home.json';
-import frHome from '../../public/locales/fr/home.json';
-import enProjects from '../../public/locales/en/projects.json';
-import frProjects from '../../public/locales/fr/projects.json';
-import enContact from '../../public/locales/en/contact.json';
-import frContact from '../../public/locales/fr/contact.json';
-import enResume from '../../public/locales/en/resume.json';
-import frResume from '../../public/locales/fr/resume.json';
-import enNavigation from '../../public/locales/en/navigation.json';
-import frNavigation from '../../public/locales/fr/navigation.json';
-import enData from '../../public/locales/en/data.json';
-import frData from '../../public/locales/fr/data.json';
+import enTranslations from '../locales/en/en.json';
+import frTranslations from '../locales/fr/fr.json';
+import enHome from '../locales/en/home.json';
+import frHome from '../locales/fr/home.json';
+import enProjects from '../locales/en/projects.json';
+import frProjects from '../locales/fr/projects.json';
+import enContact from '../locales/en/contact.json';
+import frContact from '../locales/fr/contact.json';
+import enResume from '../locales/en/resume.json';
+import frResume from '../locales/fr/resume.json';
+import enNavigation from '../locales/en/navigation.json';
+import frNavigation from '../locales/fr/navigation.json';
+import enData from '../locales/en/data.json';
+import frData from '../locales/fr/data.json';
 
 i18n
   .use(LanguageDetector)
